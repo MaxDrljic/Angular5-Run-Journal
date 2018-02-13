15d8cda01c86d60e0100feadb93dec61264c54a1
MaxDrljic <max_spree@hotmail.com>
2018-02-13 08:54:06 +0100

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ActivityListComponent } from './activity-list/activity-list.component';
 import { MapComponent } from './map/map.component';
 
 import { ActivityService } from './services/activity.service';
+import { appRoutes } from '../routes';
 
 
 @NgModule({
@@ -16,7 +19,10 @@ import { ActivityService } from './services/activity.service';
     MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ActivityService],
   bootstrap: [AppComponent]
