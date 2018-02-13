03725dca0a4298ccc4dd66f5ea40f34783eeeb72
MaxDrljic <max_spree@hotmail.com>
2018-02-13 10:06:46 +0100

    Made minor fixes in the code

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -21,28 +21,29 @@ export class MapService {
   }
 
   plotActivity(id: number) {
-    let myStyle = {
-      "color": "#3949AB",
-      "weight": 5,
-      "opacity": 0.95
+    const myStyle = {
+      'color': '#3949AB',
+      'weight': 5,
+      'opacity': 0.95
     };
 
-    let map = L.map('map').setView(defaultCoords, defaultZoom);
+    const map = L.map('map').setView(defaultCoords, defaultZoom);
 
     map.maxZoom = 100;
 
     L.tileLayer('https://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      // tslint:disable-next-line:max-line-length
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox.dark',
       accessToken: apiToken
     }).addTo(map);
 
-    let customLayer = L.geoJson(null, {
+    const customLayer = L.geoJson(null, {
       style: myStyle
     });
 
-    let gpxLayer = omnivore.gpx(SAVED_ACTIVITIES.slice(0).find(run => run.id === id).gpxData, null, customLayer)
+    const gpxLayer = omnivore.gpx(SAVED_ACTIVITIES.slice(0).find(run => run.id === id).gpxData, null, customLayer)
     .on('ready', function() {
       map.fitBounds(gpxLayer.getBounds());
     }).addTo(map);
